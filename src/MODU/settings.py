2bf444d6d3f4343f70aa22aa8868060557dda786
이승윤 <skywolf1@g.skku.edu>
2020-06-02 05:45:35 +0900

    changed template directory and static directory setting

--- src/MODU/settings.py
+++ src/MODU/settings.py
@@ -81,7 +81,7 @@ ROOT_URLCONF = 'MODU.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -159,4 +159,4 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 STATIC_URL = 'static/'
-STATIC_ROOT = posixpath.join(*(BASE_DIR.split(os.path.sep) + ['static']))
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
