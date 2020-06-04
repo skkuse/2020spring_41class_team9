70817c9283e4f04299668fa5ba7689fa751a49b8
YJY2690 <dusk2690@g.skku.edu>
2020-06-04 15:12:49 +0900

    first commit

---
+++ src/moduAssessment/models.py
@@ -0,0 +1,55 @@
+from django.db import models
+
+# Create your models here.
+
+class Assessment(models.Model):
+
+    # Fields
+    target = models.ForeignKey(
+        'Developer',
+        on_delete = models.CASCADE,
+        related_name = 'assessments',
+        related_query_name = 'assessment'
+    )
+
+    writter = models.ForeignKey(
+        'Developer',
+        on_delete = models.CASCADE,
+        related_name = 'assessments',
+        related_query_name = 'assessment'
+    )
+
+    project = models.ForeignKey(
+        'Project',
+        on_delete = models.CASCADE,
+        related_name = 'assessments',
+        related_query_name = 'assessment'
+    )
+
+    star1 = models.IntegerField(
+        verbose_name = 'ideation star'
+    )
+    star2 = models.IntegerField(
+        verbose_name = 'development star'
+    )
+    star3 = models.IntegerField(
+        verbose_name = 'communication star'
+    )
+    star4 = models.IntegerField(
+        verbose_name = 'overall star'
+    )
+
+    text = models.TextField(
+        verbose_name = 'assessment text',
+        max_length=100
+        )
+
+    # Metadata
+    class Meta: 
+        verbose_name = 'assessment'
+
+    # Methods
+    def __str__(self):
+        return self.target
+
+        
