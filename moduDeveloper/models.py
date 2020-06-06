360de365b3aaada504fa93345f29871c87b25e0d
Jae Won Lee <32070792+jwl317@users.noreply.github.com>
2020-06-06 20:46:20 +0900

    Updated
    Created validators.py too

--- moduDeveloper/models.py
+++ moduDeveloper/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from .validators import validate_file_size
 
 #Reference: https://wayhome25.github.io/django/2017/03/20/django-ep5-model/
 
@@ -8,41 +9,30 @@ class Developer(models.Model):
     """A typical class defining a model, derived from the Model class."""
 
     # Fields
-    uID = models.CharField(
+    u_ID = models.AutoField(
+        verbose_name='user ID',
+        name='uID',
         primary_key=True,
-        max_length=20,
+        unique = True,
     )
 
-    profile_image = models.FileField(
-        verbose_name='profile image',
-        upload_to="developer/profile_pic", #not sure
+    profile_image_path = models.ImageField(
+        verbose_name='upload profile image',
+        name='profile image',
+        upload_to= 'uploads/user_{0}'.format(u_ID),
+        validators=[validate_file_size],
     )
 
     portfolio = models.TextField(
         verbose_name='portfolio text',
+        name='porfolio text',
         max_length=5000,
         blank=True,
     )
 
     #Developer-Propose-Project
-    project = models.ForeignKey(
-        'Project',
-        on_delete=models.CASCADE,
-        related_name='developers',
-        related_query_name='developer'
-    )
-
-    #Developer-Write-Assessment
-    write_assessment = models.ForeignKey(
-        'Assessment',
-        on_delete=models.CASCADE,
-        related_name='developers',
-        related_query_name='developer'
-    )
-
-    #Developer-Receive-Assessment
-    receive_assessment = models.ForeignKey(
-        'Assessment',
+    proposed_projects = models.ForeignKey(
+        'moduProject.Project',
         on_delete=models.CASCADE,
         related_name='developers',
         related_query_name='developer'
@@ -50,28 +40,29 @@ class Developer(models.Model):
 
     #Many Developer-Follow-Many Developer
     follow = models.ManyToManyField(
-        'Developer',
-        related_name='developers'
-        ,related_query_name='developer'
+        "self",
+        related_name='developers',
+        related_query_name='developer'
     )
 
     #Many Developer-Member of-Many Project
     member_of = models.ManyToManyField(
-        'Project',
+        'moduProject.Project',
         related_name='developers',
         related_query_name='developer'
     )
 
     #Many Developer-Invite-Many Project
+    #unique relationship
     invite = models.ManyToManyField(
-        'Project',
+        'moduProject.Project',
         related_name='developers',
         related_query_name='developer'
     )
 
     #Many Developer-Favorite-Many Project
     favorite = models.ManyToManyField(
-        'Project',
+        'moduProject.Project',
         related_name='developers',
         related_query_name='developer'
     )
@@ -80,17 +71,15 @@ class Developer(models.Model):
     # Metadata
     class Meta:
         verbose_name = 'developer'
-        ordering = ['uID']
+        ordering = ['u_ID']
 
     # Methods
     def __str__(self):
         """String for representing the Developer object in Developer Page."""
-        return "{}\n{}\n{}\n{}\n{}\n{}\n{}\n{}\n" .format(
-            self.profile_image, 
+        return "{}\n{}\n{}\n{}\n{}\n{}\n" .format(
+            self.profile_image_path, 
             self.portfolio,
-            self.project,
-            self.receive_assessment,
-            self.write_assessment,
+            self.proposed_projects,
             self.follow,
             self.favorite,
             self.invite)
