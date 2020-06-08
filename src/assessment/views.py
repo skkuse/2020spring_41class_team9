76b40758adf7a1ef6ac3b11b5be10ab82f5b7bd9
YJY2690 <dusk2690@g.skku.edu>
2020-06-09 03:00:19 +0900

    views 임시

--- src/assessment/views.py
+++ src/assessment/views.py
@@ -1,3 +1,39 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404, redirect
+from django.views.generic import ListView
+from ..model.models import Project, Developer
 
 # Create your views here.
+
+def project_list(request):
+
+    
+    
+
+    return
+
+def project_select(request, projectID):
+
+
+
+    return
+
+
+def developer_list(request):
+    
+    
+    return
+
+def developer_select():
+
+
+
+    return
+
+
+
+
+def assessment_write():
+
+
+
+    return
