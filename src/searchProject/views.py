407cc14e05f28446b55d6c86142ec57e911af0c2
Jiyoun Kim <0715jiyounkim@gmail.com>
2020-06-12 04:02:12 +0900

    edit frontend and modified auth, search

--- src/searchProject/views.py
+++ src/searchProject/views.py
@@ -1,22 +1,22 @@
 from django.shortcuts import render
 from django.db.models import Q
-from .models import Project
-from .forms import ProjectSearchForm
+from model.models import Project
+from searchProject.forms import ProjectSearchForm
 from django.views.generic.edit import FormView
 
 # Create your views here
 class SearchProjectFormView(FormView):
     form_class = ProjectSearchForm
-    #template_name='프로젝트 검색 페이지'
+    template_name='searchProject/project.html'
 
     def form_valid(self, form):
-        search_project= self.request.POST['search_project']
+        search_project= self.request.GET['search_project']
         project_list=Project.objects.filter(Q(p_id__icontains=search_project)|Q(detailed_info__icontains=search_project)|Q(project_name__icontains=search_project)).order_by('created_time')
         context={}
         context['form']=form
         context['search_term']=search_project
         context['object_list']=project_list
 
-        return render(self.request, self.#teamplate_name, 
+        return render(self.request, self.teamplate_name, 
         context)
    
