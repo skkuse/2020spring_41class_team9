0ebb2009e5830c3a9805e6cf683f47900c03f332
YJY2690 <dusk2690@g.skku.edu>
2020-06-11 21:04:34 +0900

    front end 임시

--- src/assessment/views.py
+++ src/assessment/views.py
@@ -8,22 +8,26 @@ from .forms import AssessmentForm
 #상호평가 버튼 클릭시
 def project_list(ListView):
 
-    context_object_name = 'project_list'
+    context_object_name = 'assessment_project_list'
+    template_name = 'assessment/project_list.html'
+    paginate_by = 10
     
     def get_queryset(self):
-        queryset = Developer.objects.filter(u_id = self.request.session.get('u_id')).value('member_of')
+        queryset = Developer.objects.filter(u_id = self.request.session.get('u_id'))#.value('member_of')
         return queryset
 
 #프로젝트 선택시
 def developer_list(request, projectID):
 
-    context_object_name = 'developer_list'
+    context_object_name = 'assessment_developer_list'
+    template_name = 'assessment/developer_list.html'
     project = get_object_or_404(Project, p_id = projectID)
 
     def get_queryset(self):
         queryset = Developer.objects.all()
         queryset = queryset.filter(member_of = project)
-        queryset = queryset.exclude(u_id = self.request.session.get('u_id')).valude('name') #developer model에 name 필드 추가?
+        queryset = queryset.exclude(u_id = self.request.session.get('u_id'))#.valude('name') 
+        #developer model에 name 필드 추가?
 
         return queryset
     
@@ -40,3 +44,5 @@ def assessment(request, projectID, developerID):
     else:
         form = AssessmentForm()
         return render(request, 'assessment.html', {'form':form})
+
+
