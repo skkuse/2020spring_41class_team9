9a0facedf8ff88f59b9819bc752eec84ec6cfefe
YJY2690 <dusk2690@g.skku.edu>
2020-06-12 04:16:05 +0900

    followModel, favoriteModel 반영

--- src/favorite_follow/views.py
+++ src/favorite_follow/views.py
@@ -1,32 +1,29 @@
 from django.shortcuts import render, get_object_or_404, redirect
 from django.contrib.auth.models import User
-from ..model.models import Project, Developer
+from model.models import Project, Developer
 from django.contrib.auth.decorators import login_required
 from django.http import HttpResponse
 import json
 
-
+@login_required
 def favorite(request):
     project = get_object_or_404(Project, request.POST['p_id'])
     u_id = request.session['u_id']
     developer = Developer.object.get(u_id = u_id)
 
-    check = developer.favorite.filter(p_id = project.p_id)
+    #check = developer.favorite.filter(p_id = project.p_id)
+    fav , flag = favoriteModel.object.get_or_create(favorite = project, developer = developer)
 
-    if check.exists():
+    if not flag:
         developer.favorite.remove(project)
-        #project.favorite_count -= 1 
-        #project.save()
+        fav.delete()
         message = "favorite"
     else :
         developer.favorite.add(project)
-        #project.favorite_count -= 1 
-        #project.save()
         message = "cancel favorite"
 
     context = {
         'message' : message,
-        #'num' : project.favorite_count(),
     }
 
     return HttpResponse(json.dumps(context), content_type= "application/json")
@@ -36,12 +33,17 @@ def favorite(request):
 def follow(request):
 
     target = get_object_or_404(Developer, request.POST['target_id'])
-    developer = get_object_or_404(Developer, u_id = request.session['u_id'])
+    u_id = request.session['u_id']
+    developer = get_object_or_404(Developer, u_id = u_id)
 
-    check = developer.follow.filter(u_id = request.POST['target_id'])
+    #check = developer.follow.filter(u_id = request.POST['target_id'])
 
-    if check.exists():
+    fol , flag = followModel.object.get_or_create(follower = developer, followee = target)
+
+
+    if not flag:
         developer.follow.remove(target)
+        fol.delete()
         message ="follow"
     else :
         developer.follow.add(target)
