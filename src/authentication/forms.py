5e25c06b3c150d0abc6094f54e5a7aa9e4bfa393
이승윤 <skywolf1@g.skku.edu>
2020-06-12 00:32:36 +0900

    set_unusable_password on user creation

--- src/authentication/forms.py
+++ src/authentication/forms.py
@@ -143,7 +143,7 @@ class CustomUserCreationForm(forms.ModelForm):
 
     def save(self, commit=True):
         user = super().save(commit=False)
-        user.set_password(self.cleaned_data["password1"])
+        user.set_unusable_password()
         if commit:
             user.save()
         return user
