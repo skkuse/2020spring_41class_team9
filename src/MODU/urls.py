efb1102b0a7175a0d68ddeeef639f0f77ecb4ec4
YJY2690 <dusk2690@g.skku.edu>
2020-06-12 02:38:30 +0900

    .

--- src/MODU/urls.py
+++ src/MODU/urls.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 from django.urls import path
-import assessment.views
+import project.views
 
 # Uncomment next two lines to enable admin:
 #from django.contrib import admin
@@ -9,11 +9,5 @@ import assessment.views
 
 urlpatterns = [
     # Uncomment the next line to enable the admin:
-    path('admin/', admin.site.urls),
-
-    #assessment
-    path('mypage/assessment/', assessment.views.project_list.as_view(), name = 'project_list'),
-    path('mypage/assessment/<int:p_id>', assessment.views.developer_list.as_view(), name = 'developer_list'),
-    path('mypage/assessment/<int:p_id>/<int:u_id>', assessment.views.assessment, name = 'assessment')
-
+    #path('admin/', admin.site.urls)
 ]
