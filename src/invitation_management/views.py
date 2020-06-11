5ac6926c64fd80a4bf0f98dbd6a4263d506983da
jwl317 <jwl_17@naver.com>
2020-06-11 20:28:27 +0900

    updated views.py for sending, accepting and rejecting invitation
    Signed-off-by: jwl317 <jwl_17@naver.com>

--- src/invitation_management/views.py
+++ src/invitation_management/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render, get_object_or_404
 from django.contrib.auth.models import User
 from .models import Project, Developer, Invitation
+from django.http import HttpResponse
+import json
 
 # previous change: invitation inquiry is done through message
 # so here, we only deal with (1) developer inviting other developer in a project,
@@ -10,36 +12,53 @@ from .models import Project, Developer, Invitation
 
 def invite(request, userID):
     
-    developer = get_object_or_404(Developer, userID)
-    u_id = request.u_id
-    project = Project.object.get(u_id = u_id)
-    check = Invitation.object.get(p_id = p_id)
+    subject = get_object_or_404(Developer, request.POST[userID])
+    developer = get_object_or_404(Developer, u_id = request.session['u_id'])
+    
+    check = developer.invite.filter(u_id = request.POST[userID])
 
     if check.exists():
-        check.invite.remove(project)
+        developer.invite.remove(subject)
+        message = "invite"
+
+    else :
+        developer.invite.add(subject)
+        message = "cancel invite"
+
+    context = {
+        'message' : message,    
+    }
+
+    project = get_object_or_404(Project, request.POST['p_id'])
+    u_id = request.session['u_id']
+    developer = Developer.object.get(u_id = u_id)
+
+    #project = Project.object.get(p_id = p_id)
+
+    check = Invitation.object.filter(invited_pid = project.p_id)
+
+    if check.exists():
+        Invitation.object.remove(project)
     else:
-        check.invite.add(project)
+        Invitation.object.add(project)
 
     return redirect('/developer/'+str(userID))
 
 def invitationAccept(request, userID):
     
-    invitation = get_object_or_404(Invitation, userID)
-    u_id = request.u_id
-    i_id = request.i_id
-    check = invitation.object.get(i_id = i_id)
+    invitation = get_object_or_404(Invitation, request.POST['i_id'])
+    i_id = request
+    u_id = request.session['u_id']
 
-    # 이미 수락한거라면 초대 리스트에서 삭제
-    if check.exist():
+    check = invitation.object.filter(i_id = i_id)
+
+    # Accept
+    if check.exists():
         check.is_accepted = TRUE
         return check.remove(i_id)
-
+    # Reject
     else:
-        # 초대 거절후 초대 리스트에서 삭제
-        # accepted = FALSE
-        return invitation.remove(i_id)
-
-        # 초대 아무 반응 안할시 그대로 리턴
-        
+        check.is_accepted = FALSE
+        return
     
     return redirect('/mypage/invitation/'+str(userID))
