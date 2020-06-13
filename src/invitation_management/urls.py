fe369141d445830879cec2b66cadd40da47e6822
jwl317 <jwl_17@naver.com>
2020-06-13 23:38:29 +0900

    updated urls.py and edited invite_accept() and invite_reject()
    Signed-off-by: jwl317 <jwl_17@naver.com>

---
+++ src/invitation_management/urls.py
@@ -0,0 +1,9 @@
+  
+from django.contrib import admin
+from django.urls import path
+from invitation.views import *
+
+urlpatterns = [
+    path('invitation_management/', InviteListView.as_view(), name='InviteList'),
+
+]
--- src/invitation_management/views.py
+++ src/invitation_management/views.py
@@ -16,35 +16,39 @@ class InviteListView(ListView):
         queryset = Invitation.objects.filter(receiver = developer)
 
         return queryset.order_by('sent_time')
-    
-def invite_accept(request, project, uID):
 
-    context_object_name = 'invite_list'
-    template_name = 'searchDeveloper/invitationlist.html'
+@login_required   
+def invite_accept(request, pID, uID):
 
-    def get_queryset(self, **kwargs):
-        developer = get_object_or_404(Developer, uID = self.request.session.get('uID'))
-        queryset = Invitation.objects.filter(receiver = developer)
-
-        #Invitation의 project를 developer의 member_of에 추가하고
+    if request.method == "POST":
+        developer = get_object_or_404(Developer, uID = uID)
+        project = get_object_or_404(Project, pID = pID)
+        invitation = get_object_or_404(Invitation, receiver = developer)
+        
         developer.member_of.add(project)
+        #invitation.is_accepted = True
+        #invitation.save()
+        invitation.project.remove(project)
 
-        queryset = queryset.exclude(project) #?
+        return redirect('InviteList')
+    
+    else:
+        return render(request, 'searchDeveloper/invitationlist.html')
 
-        return queryset.order_by('sent_time')
 
+@login_required
+def invite_reject(request, pID, uID):
 
-# 초대 거절 선택시
-def invite_reject(request, project, uID):
-
-    context_object_name = 'invite_list'
-    template_name = 'searchDeveloper/invitationlist.html'
-
-    def get_queryset(self, **kwargs):
-        developer = get_object_or_404(Developer, uID = self.request.session.get('uID'))
-        queryset = Invitation.objects.filter(receiver = developer)
-
-        #Invitation 제거
-        queryset.is_accepted = False #?
-
-        return queryset.order_by('sent_time')
+    if request.method == "POST":
+        developer = get_object_or_404(Developer, uID = uID)
+        project = get_object_or_404(Project, pID = pID)
+        invitation = get_object_or_404(Invitation, receiver = developer)
+        
+        invitation.project.remove(project)
+        #invitation.is_accepted = False
+        #invitation.save()
+        return redirect('InviteList')
+    
+    else:
+        return render(request, 'searchDeveloper/invitationlist.html')
+      
