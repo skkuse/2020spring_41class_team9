2b157b289d92fbfaefa8c98764d1c75fa3e940a3
YJY2690 <dusk2690@g.skku.edu>
2020-06-13 23:33:06 +0900

    uID, pID

--- src/assessment/urls.py
+++ src/assessment/urls.py
@@ -4,6 +4,6 @@ from .views import *
 
 urlpatterns = [
     path('', project_list.as_view(), name = "assessment_project_list"),
-    path('<int:p_id>', developer_list.as_view(), name = "assessment_developer_list"),
-    path('<int:p_id>/<int:u_id>', assessment, name= "assessment")
+    path('<int:pID>', developer_list.as_view(), name = "assessment_developer_list"),
+    path('<int:pID>/<int:uID>', assessment, name= "assessment")
     ]
