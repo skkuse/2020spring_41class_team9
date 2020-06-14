f47cca5c09fe2383f3fa0fbed148ecf792a4c98d
이승윤 <skywolf1@g.skku.edu>
2020-06-14 21:11:12 +0900

    solved bug

--- src/portfolio/views.py
+++ src/portfolio/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render, get_object_or_404, redirect
 from django.contrib.auth.decorators import login_required
 from django.views.generic.edit import UpdateView
 from model.models import Developer
-from project.form import PortfolioPost
+from portfolio.form import PortfolioPost
 from django.utils.decorators import method_decorator
 from django.views.decorators.http import require_http_methods
 from django.contrib.auth.mixins import UserPassesTestMixin
