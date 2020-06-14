f00188087c9c7dd798a7d5e81790d3194683a385
YJY2690 <dusk2690@g.skku.edu>
2020-06-14 18:35:52 +0900

    follow 부분만

--- src/information_view/urls.py
+++ src/information_view/urls.py
@@ -5,4 +5,7 @@ from information_view.views import *
 urlpatterns = [
     path('project/<pk>', Project_detail_view.as_view()),
     path('developer/<pk>', Developer_detail_view.as_view()),
+    
+    path('favorite/<int:pID>/', Favorite.as_view(), name='favorite'),
+    path('developer/<int:uID>/', Follow.as_view(), name='follow'),
 ]
--- src/information_view/views.py
+++ src/information_view/views.py
@@ -1,7 +1,13 @@
 from django.shortcuts import render, get_object_or_404
 from django.contrib.auth.models import User
-from model.models import Project, Developer
+from model.models import Project, Developer, followModel
 from django.views.generic import DetailView
+
+from django.views.generic.base import View
+from django.http import HttpResponseForbidden, HttpResponseRedirect
+from urllib.parse import urlparse
+from django.utils.decorators import method_decorator
+from django.contrib.auth.decorators import login_required
 
 
 # Create your views here.
@@ -23,3 +29,47 @@ class Developer_detail_view(DetailView):
     template_name = "information_view/developerinfo.html"
     context_object_name = "developer"
 
+
+
+@method_decorator(login_required, name='dispatch')
+class Favorite(View) :
+    def get(self, request, *args, **kwargs):
+        if 'pID' in kwargs:
+            pID = kwargs['pID']
+            project = Project.object.get(pID = pID)
+            uID = request.session['uID']
+            user = Developer.object.get(uID = uID)
+            
+            fav , flag = favoriteModel.object.get_or_create(favorite = project, developer = user)
+
+            if not flag:
+                user.favorite.remove(project)
+                fav.delete()
+            else:
+                user.favorite.add(user)
+
+        referer_url = request.META.get('HTTP_REFERER')
+        path = urlparse(referer_url).path
+        return HttpResponseRedirect(path)
+
+
+@method_decorator(login_required, name='dispatch')
+class Follow(View) :
+    def get(self, request, *args, **kwargs):
+        if 'uID' in kwargs:
+            followee_ID = kwargs['uID']
+            followee = get_object_or_404(Developer, uID = followee_ID)
+            kk = request.user
+            user = get_object_or_404(Developer, uID = kk.uID)
+            
+            fol , flag = followModel.objects.get_or_create(follower = user, followee = followee)
+
+            if not flag:
+                user.follow.remove(followee)
+                fol.delete()
+            else:
+                user.follow.add(followee)
+
+        referer_url = request.META.get('HTTP_REFERER')
+        path = urlparse(referer_url).path
+        return HttpResponseRedirect(path)
